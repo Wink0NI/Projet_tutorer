790b2d5e17239d38e55abbcd75faa828dfa558d1
Wink0NI <138539365+Wink0NI@users.noreply.github.com>
2024-09-12 10:41:17 +1100

    fix

--- functions/edit_str.py
+++ functions/edit_str.py
@@ -1,9 +1,9 @@
 # Retourner une valeur string en Int ou Float
 def str_to_none(value):
-    return None if value == '' else value
+    return None if value == 'NULL' else value
 
 def str_to_nbr(value):
     try:
         return float(value) if '.' in value else int(value)
-    except ValueError:
+    except:
         return value
