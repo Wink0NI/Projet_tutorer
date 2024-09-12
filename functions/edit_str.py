26b0243974b43fcbe39720be06da711b86f7504f
Ay0li <olivier.tram.pro@gmail.com>
2024-09-12 11:36:56 +1100

    Update edit_str.py

--- functions/edit_str.py
+++ functions/edit_str.py
@@ -14,6 +14,6 @@ def convert_custom_datetime(date_str):
     try:
         # Convertir le format '04-24T17:30Z' en '2024-04-24 17:30:00'
         dt = datetime.strptime(date_str, '%m-%dT%H:%MZ')
-        return dt.strftime('%Y-%m-%d %H:%M:%S')
+        return dt.strftime("%Y-%m-%d %H:%M:%S") # SUR MACOS mettre "" au lieu de ''
     except:
         return None# Retourner une valeur string en Int ou Float
