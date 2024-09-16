d74c502d90a92e6b873876867d9c6cb04f238b6c
Wink0NI <138539365+Wink0NI@users.noreply.github.com>
2024-09-17 09:58:58 +1100

    trajectoire bateau

--- queries/query_1.py
+++ queries/query_1.py
@@ -1,6 +1,8 @@
-# Connexion à la base de données PostgreSQL
 import psycopg2
+import folium
+from datetime import datetime
 
+# Connexion à la base de données PostgreSQL
 conn = psycopg2.connect(
     host="localhost",
     database="Projet_tutorer",
@@ -9,14 +11,58 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM ais_positions_noumea")
+# Exécuter la requête SQL pour obtenir les positions du bateau le 20 avril 2024
+cur.execute("""
+    SELECT apn.lat, apn.lon, apn.received_at, shipname
+    FROM ais_positions_noumea apn
+    JOIN ais_information_vessel aiv ON apn.mmsi = aiv.mmsi
+    WHERE apn.mmsi = 209591000
+    AND apn.received_at::date = '2024-03-14'
+    ORDER BY apn.received_at
+""")
 
 # Récupérer toutes les lignes de la requête
 rows = cur.fetchall()
 
-# Afficher les résultats
-for row in rows:
-    print(row)
-
+# Fermer la connexion à la base de données
 cur.close()
 conn.close()
+
+# Créer une carte centrée sur la première position du bateau
+if rows:
+    first_position = rows[0]
+    carte = folium.Map(location=[first_position[0], first_position[1]], zoom_start=12)
+
+    # Ajouter une ligne pour représenter le trajet du bateau
+    trajectory = []
+    for row in rows:
+        lat, lon, received_at, shipname = row
+        trajectory.append((lat, lon))
+        # Convertir datetime en chaîne de caractères
+        received_at_str = received_at.strftime('%Y-%m-%d %H:%M:%S')
+        # Ajouter un marqueur pour chaque position avec popup stylisé
+        folium.Marker(
+            location=[lat, lon],
+            popup=f"""
+                <div style="width: 200px; white-space: nowrap;">
+                    <b>Nom:</b> {shipname}</br>
+                    <b>Heure:</b> {received_at_str}<br>
+                    <b>Latitude:</b> {lat}<br>
+                    <b>Longitude:</b> {lon}
+                </div>
+            """,
+            icon=folium.Icon(color='blue', icon='ship')
+        ).add_to(carte)
+
+    # Ajouter une ligne pour le trajet
+    folium.PolyLine(
+        locations=trajectory,
+        color='blue',
+        weight=2.5,
+        opacity=0.8
+    ).add_to(carte)
+
+    # Afficher la carte
+    carte.save('trajet_bateau.html')  # Sauvegarder en HTML pour l'afficher dans un navigateur
+else:
+    print("Aucune donnée disponible pour le bateau avec MMSI 209135000 le 20 avril 2024.")
